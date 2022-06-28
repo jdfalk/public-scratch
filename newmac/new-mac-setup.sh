c81f716f73d34af961461204003f813ced9a11d8
Johnathan Falk <johnathan.falk@gmail.com>
2022-06-28 10:48:25 -0400

    Lots of changes to make the file work better.
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- newmac/new-mac-setup.sh
+++ newmac/new-mac-setup.sh
@@ -6,20 +6,26 @@
 echo "Welcome to the mac setup script. This was created to simplify initial setup."
 
 
-# Install command line tools
-xcode-select --install
+DEBUG=0
 
+# Install command line tools
+if $DEBUG; then
+	echo "Installing command line tools"
+fi
+
+xcode-select --install
 if [ $? -eq 0 ]
 then
-  echo "Successfully installed command line tools"
+	echo "Successfully installed command line tools"
 else
-  echo "Failed to install command line tools" >&2
+	echo "Failed to install command line tools" >&2
 fi
 
 
 if [ ! -d /Applications/iterm2.app ]; then
-  curl -L https://iterm2.com/downloads/stable/latest -o /Applications/iterm2.zip
-  unzip /Applications/iterm2.zip
+	curl -L https://iterm2.com/downloads/stable/latest -o /Applications/iterm2.zip
+	unzip /Applications/iterm2.zip
+	rm /Applications/iterm2.zip
 fi
 
 # Create my GIT stuff
@@ -34,21 +40,34 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 ln -s $HOME/repos/github/jdfalk/public-scratch/.istio.zsh.inc $HOME/.istio.zsh.inc
 ln -s $HOME/repos/github/jdfalk/public-scratch/.kube.zsh.inc $HOME/.kube.zsh.inc
-ln -s $HOME/repos/github/jdfalk/public-scratch/.aws.zsh.inc $HOME/.aws.zsh.inc
+# ln -s $HOME/repos/github/jdfalk/public-scratch/.aws.zsh.inc $HOME/.aws.zsh.inc
 
 cat <<'EOF' >> ~/.zshrc
 if [ -f ~/.kube.zsh.inc ]; then
-    source ~/.kube.zsh.inc
+		source ~/.kube.zsh.inc
 fi
 
 if [ -f ~/.istio.zsh.inc ]; then
-    source ~/.istio.zsh.inc
+		source ~/.istio.zsh.inc
 fi
 
 if [ -f ~/.aws.zshrc.inc ]; then
-    source ~/.aws.zshrc.inc
+		source ~/.aws.zshrc.inc
 fi
 EOF
+
+
+if [[ $(uname -m) == 'arm64' ]]; then
+	cat >> ~/.zshrc <<-EOL
+		# Homebrew on M1
+		export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+	EOL
+elif [[ $(uname -m) == 'x86_64' ]]; then
+	cat >> ~/.zshrc <<-EOL
+		# Homebrew on Intel
+		export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+	EOL
+fi
 
 
 # Setup brew
@@ -61,55 +80,74 @@ brew tap argoproj/tap
 brew tap aws/tap
 brew tap boz/repo
 brew tap go-delve/delve
+brew tap homebrew/bundle
 brew tap homebrew/cask
 brew tap homebrew/core
-brew tap puppetlabs/puppetlabs
+brew tap homebrew/services
+# brew tap puppetlabs/puppetlabs
 brew update
 
 # Install basic software
 brew install \
-    awscli \
-    azure-cli \
-    bash-completion \
-    coreutils \
-    fortune \
-    gnu-sed \
-    gnu-tar \
-    gnupg \
-    go \
-    helm \
-    iperf3 \
-    istioctl \
-    jq \
-    kubectx \
-    kubernetes-cli \
-    kube-ps1 \
-    openssl \
-    packer \
-    prometheus \
-    pyenv \
-    pyenv-virtualenv \
-    rclone \
-    stern \
-    terraform \
-    terraform-docs \
-    terragrunt \
-    zsh-syntax-highlighting \
-    zsh-autosuggestions 
+		awscli \
+		azure-cli \
+		bash-completion \
+		coreutils \
+		fortune \
+		gnu-sed \
+		gnu-tar \
+		gnupg \
+		go \
+		helm \
+		iperf3 \
+		istioctl \
+		jq \
+		k9s \
+		kube-ps1 \
+		kubectx \
+		kubernetes-cli \
+		mas \
+		openssl \
+		packer \
+		prometheus \
+		pyenv \
+		pyenv-virtualenv \
+		rclone \
+		stern \
+		terraform \
+		terraform-docs \
+		terragrunt \
+		zsh-syntax-highlighting \
+		zsh-autosuggestions 
 
-brew cask install puppetlabs/puppet/pdk
+brew cask install \
+	keybase \
+	lens \
+	visual-studio-code
+
+# 1Password 7
+mas install 1333542190
+# Microsoft Remove Desktop
+mas install 1295203466
+# Workspot
+mas install 978894159
+
 
 # Setup GIT
 # git config --global user.name ""
 # git config --global user.email ""
 
 
+pyenv install 3.10.4
+pyenv virtualenv 3.10.4 g3.10.4
+pyenv global g3.10.4
 
+cat <<'EOF' >> ~/.zshrc
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+EOF
 
-
-
-
-echo "Remember to install vscode and settings sync"
 
 ## Plugins
 
@@ -117,11 +155,11 @@ echo "Remember to install vscode and settings sync"
 echo "Installing Krew..."
 
 (
-  set -x; cd "$(mktemp -d)" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
-  tar zxvf krew.tar.gz &&
-  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
-  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
-  "$KREW" update
+	set -x; cd "$(mktemp -d)" &&
+	curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
+	tar zxvf krew.tar.gz &&
+	KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
+	"$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
+	"$KREW" update
 )
 
